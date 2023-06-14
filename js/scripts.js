cd9d647b9ce794439b8b80aa06b34575b73cb9d5
yehric2018 <eric.yehj@outlook.com>
2023-06-14 09:30:33 -0700

    Starting from scratch

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $(document).getElementsByClassName('blog-content')[0].innerHTML = "KABOOM";
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-};
